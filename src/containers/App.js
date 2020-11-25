50ba468c40fa22a14a0c698589b215f0ae7ed98b
lcs930415 <lcs930415@gmail.com>
2020-11-24 20:03:13 -0500

    Creation Lifecycle

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state={
     persons:[
       {id:'1',name:'Max',age: 28},
@@ -13,6 +18,19 @@ class App extends Component {
     otherState: 'some other value',
     username: 'supermax',
     showPersons:false,
+  }
+
+  static getDerivedStateFromProps(props,state){
+    console.log('[App.js] getDerivedStateFormProps', props);
+    return state;
+  }
+
+  // componentwillMount(){
+  //   console.log('[App.js] componentWillMount');
+  // }
+
+  componentDidMount(){
+    console.log('[App.js] componentDivMount');
   }
 
   deletePersonHandler =(personIndex) =>{
@@ -45,7 +63,9 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
+
     if (this.state.showPersons){
       persons = <Persons 
             persons={this.state.persons}
